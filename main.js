d224fe6d8e8dc44fc27d1c4579fe0d46329c8a98
Mujahid Abdhamid <86028556+Mujahid-Abdhamid@users.noreply.github.com>
2024-02-21 23:09:12 +0100

    Building the mobile nav

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const dropDown = document.querySelectorAll(".dropdown");
 const btnOpen = document.querySelectorAll(".nav-list__item");
 const closeIcon = document.querySelectorAll(".close");
 const openIcon = document.querySelectorAll(".open");
+const menu = document.querySelector(".menu");
+const navBar = document.querySelector(".main-nav");
 
 // Implementing the open functionality and close functionality
 btnOpen.forEach((open, index) => {
@@ -36,3 +38,39 @@ btnOpen.forEach((open, index) => {
     openIcon[index].classList.remove("hidden");
   });
 });
+
+const tabletScreen = window.matchMedia("(max-width: 59em)");
+
+if (tabletScreen.matches) {
+  menu.classList.remove("hidden");
+  menu.addEventListener("click", function () {
+    navBar.style.opacity = "2";
+  });
+}
+
+/* .message {
+  display: inline-block;
+  background-color: hsl(0, 0%, 98%);
+  box-shadow: 0 1.2rem 2.4rem rgba(0, 0, 0, 0.15);
+  border-radius: 0.5rem;
+  padding: 2.4rem;
+
+  position: relative;
+}
+
+.message::before {
+  content: "";
+  width: 2.5rem;
+  height: 2.5rem;
+  position: absolute;
+  top: 0;
+  right: -12px;
+  background: linear-gradient(
+    135deg,
+    hsl(0, 0%, 98%) 0%,
+    hsl(0, 0%, 98%),
+    50%,
+    transparent 50%,
+    transparent
+  );
+} */
